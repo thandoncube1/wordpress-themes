4ce3b4c46b78eaa6f448c411249bc779e0ea64fb
thandoncube1 <ncubethando20@gmail.com>
2025-05-29 03:08:52 -0400

    Working on the altair design - homepage

--- themes/altair/footer.php
+++ themes/altair/footer.php
@@ -1,6 +1,6 @@
 </main>
 <footer>
-    <p>Altair Inc. &copy; <?php date('Y'); ?> All Rights Reserved.</p>
+    <p>Altair Inc. &copy; <?php echo date('Y'); ?> &#151; All Rights Reserved.</p>
     <?php wp_footer(); ?>
 </footer>
 </body>
--- themes/altair/index.php
+++ themes/altair/index.php
@@ -1,6 +1,40 @@
 <?php get_header(); ?>
-    <main>
+<main>
+    <section class="main-header">
+        <span><strong>New:</strong> Infinite Canvas <a href="#">&nbsp;Learn more</a></span>
+        <h1>Super fast motion <br> for every team</h1>
+        <button>Try Altair for free</button>
+        <section class="promotions">
+            <hr>
+            <span><strong>Over 20,000 creative teams use Altair</strong> to create stunning animations online.</span>
+            <article class="companies">
+                <div class="image-container"><img src="<?php echo get_theme_file_uri('assets/images/google.svg'); ?>" alt="Google logo" srcset="" class="image-animation"></div>
+                <div class="image-container"><img src="<?php echo get_theme_file_uri('assets/images/akqa.svg'); ?>" alt="Akqa logo" srcset="" class="image-animation"></div>
+                <div class="image-container"><img src="<?php echo get_theme_file_uri('assets/images/deliveroo.svg'); ?>" alt="Deliveroo logo" srcset="" class="image-animation"></div>
+                <div class="image-container"><img src="<?php echo get_theme_file_uri('assets/images/perplexity.svg'); ?>" alt="Perplexity logo" srcset="" class="image-animation"></div>
+                <div class="image-container"><img src="<?php echo get_theme_file_uri('assets/images/tiktok.svg'); ?>" alt="Tiktok logo" srcset="" class="image-animation"></div>
+                <div class="image-container"><img src="<?php echo get_theme_file_uri('assets/images/webflow.svg'); ?>" alt="Webflow image" srcset="" class="image-animation"></div>
+                <div class="image-container"><img src="<?php echo get_theme_file_uri('assets/images/ramp.svg'); ?>" alt="Ramp logo" srcset="" class="image-animation"></div>
+            </article>
+        </section>
+    </section>
+    <section class="altair-content__video">
+        <article class="altair__video">
+            <video autoplay loop src="<?php echo get_theme_file_uri('assets/videos/video-hero-hd-20.mp4'); ?>"></video>
+        </article>
+    </section>
+    <section class="altair-content__tagline">
+        <div class="scoll-to-appear">
+            <?php
+                $text = "Altair makes motion accessible to every designer, enabling creative teams to collaborate on and deliver engaging animations in record time";
 
-<?php
+                $words = explode(" ", $text);
+                foreach ($words as $word) {
+                    echo "<div>" . trim($word) . "</div>";
+                }
+            ?>
+        </div>
+    </section>
+    <?php
     get_footer();
-?>
+    ?>
