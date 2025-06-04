c44b04d13d2b492fb091446b6d9b23a73c6acafa
thandoncube1 <ncubethando20@gmail.com>
2025-06-04 18:18:37 -0400

    Adding the footer in on the main page

--- themes/altair/footer.php
+++ themes/altair/footer.php
@@ -1,8 +1,40 @@
 </main>
 <footer>
-    <p>Altair Inc. &copy; <?php echo date('Y'); ?> &#151; All Rights Reserved.</p>
-    <?php wp_footer(); ?>
+    <section class="container">
+        <section class="row">
+            <article class="footer-col">
+                <h1><a href="<?php echo home_url(); ?>"><strong>Altair</strong>Custom</a></h1>
+            </article>
+            <article class="footer-col">
+                <h2>Products</h2>
+                <section>
+                    <p><a href="#">Import from Figma</a></p>
+                    <p><a href="#">Design</a></p>
+                    <p><a href="#">Animate</a></p>
+                    <p><a href="#">Collaborate</a></p>
+                </section>
+            </article>
+            <article class="footer-col">
+                <h2>Solutions</h2>
+                <section>
+                    <p><a href="#">Devices</a></p>
+                    <p><a href="#">Text Animations</a></p>
+                    <p><a href="#">Logos</a></p>
+                    <p><a href="#">Animated Charts</a></p>
+                    <p><a href="#">Animated Websites</a></p>
+                </section>
+            </article>
+            <article class="footer-col">
+                <h2>Contact Info</h2>
+                <section>
+                    <p><a href="#">Contact Sales</a></p>
+                    <p><a href="#">Technical Support</a></p>
+                </section>
+            </article>
+        </section>
+    </section>
 </footer>
+<?php wp_footer(); ?>
 </body>
 
 </html>
